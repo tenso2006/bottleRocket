c2767607705b72fc84fb01df28008561eb58f8f1
Tenzin Sonam <tenso2006@gmail.com>
2018-03-25 12:17:29 -0400

    New - react router and other components added

--- bottlerocket/src/App.js
+++ bottlerocket/src/App.js
@@ -1,62 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import previous from './Cuts/ic_webBack@2x.png';
-import refresh from './Cuts/ic_webRefresh@2x.png';
-import next from './Cuts/ic_webForward@2x.png';
-import '../node_modules/font-awesome/css/font-awesome.min.css';
-import grouppic from './grouppic.jpg';
-import grp1 from './grp1.png';
-import group1 from './group1.jpg';
-import mainPic from './main-pic.png';
-import bottleRocketLogo from './bottleRocketLogo.png';
-import lunchIcon from './Cuts/tab_lunch@2x.png';
-import internetsIcon from './Cuts/tab_internets@2x.png';
+//import './App.css';
+//import { Router, Route } from 'react-router';
+import {BrowserRouter, Route} from 'react-router-dom';
+import Home from './Components/Home.js';
+import Restaurant from './Components/Restaurant.js';
+import Lunch from './Components/Lunch.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className='top-nav'>
-          <div className='control-button'>
-            <i className="fa fa-angle-left fa-3x"></i>
-            <i className="fa fa-rotate-left fa-2x"></i>
-            <i className="fa fa-angle-right fa-3x"></i>
-          </div>
+      <BrowserRouter>
+        <div>
+          <Route path='/' component={Home} />
+          <Route path='/restaurant' component={Restaurant} />
+          <Route path='/lunch' component={Lunch} />
         </div>
-        <div className='head-nav'>
-          <div className='hambugger-icon'>
-            <i className="fa fa-bars"></i>
-          </div>
-          <div className='contact'>contact</div>
-          <div className='brand-logo'>
-            <img src={bottleRocketLogo} />
-          </div>
-        </div>
-        <div className='main-body'>
-          <div className='main-pic'>
-            <div className='back-image' style={{backgroundImage: 'url(' + mainPic + ')'}}>
-              {/*<img src={grouppic} className='grouppic' alt='group pictures' style={width:'128px', height:'128px'}/>*/}
-              <div>we look forward to hearing from you</div>
-            </div>
-          </div>
-          <div className='body-content'>
-            <div className='body-content-head'><h4>Let us know how we can help!</h4></div>
-
-            <p className='body-content-detail'>If you are interested in hiring Bottle Rocket for your mobile project, please visit our hire us page.
-            </p>
-            <hr />
-          </div>
-        </div>
-        <div className='footer'>
-          <div className='lunch-icon'>
-            <img src={lunchIcon} />
-          </div>
-          <div className='internets-icon'>
-            <img src={internetsIcon} />
-          </div>
-        </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
